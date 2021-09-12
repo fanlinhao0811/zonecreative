7796ee1b975e4e2e0c2655a6022de8a4194ecd22
linhao.fan <haohaoangle@163.com>
2021-09-12 23:03:14 +0800

    add swiper

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -15,6 +15,73 @@ li.btn-active {
 .swiper-logo {
   overflow: hidden;
 }
+.hover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1042;
+  overflow: hidden;
+  background: rgba(11, 11, 11, 0.8);
+  &-container {
+    width: 772px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1045;
+    &-close {
+      text-align: right;
+      cursor: pointer;
+      font-weight: bolder;
+      color: #fff;
+    }
+    &-main {
+      width: 772px;
+      height: 514px;
+      background: #ccc;
+    }
+    &-wrapper {
+      width: 100%;
+      height: 260px;
+      background: #fff;
+      border-radius: 0px;
+      padding: 16px 32px;
+      border: 0px;
+      &-title {
+        font-size: 24px;
+        /* margin-bottom: 50px; */
+        color: #222222;
+        margin: 0;
+        font-weight: 700;
+      }
+      &-desc {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 4;
+        -webkit-box-orient: vertical;
+        font-size: 15px;
+        color: #727272;
+        line-height: 2;
+        margin: 16px 0;
+      }
+      &-date {
+        font-size: 12px;
+        font-weight: 700;
+        color: #222222;
+        display: inline-block;
+        padding: 2px 0;
+      }
+    }
+  }
+  .swiper-slide-img {
+    width: 772px;
+    height: 514px;
+    object-fit: cover;
+  }
+}
 </style>
 
 <template>
@@ -175,7 +242,8 @@ li.btn-active {
               <div class="project-content project-content-hover">
                 <h2>{{ item.name }}</h2>
                 <p>{{ item.tags[0] }}</p>
-                <a href="#"
+                <a href="javascript:void(0);"
+                   @click="show(item)"
                    class="seemore">See Project </a>
               </div>
             </div>
@@ -208,6 +276,40 @@ li.btn-active {
         </div>
       </div>
     </div>
+
+    <div class="hover"
+         v-if="showHover">
+      <div class="hover-container">
+        <div class="hover-container-close"
+             @click="closeHover">
+          X
+        </div>
+        <div class="hover-container-main">
+          <div class="swiper-container-hover">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide"
+                   v-for="(item, index) in showPottfolio.images"
+                   :key="index">
+                <img :src="`https://z1creative.com/upload/${item.filename}`"
+                     alt=""
+                     class="swiper-slide-img">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="hover-container-wrapper">
+          <div class="hover-container-wrapper-title">
+            {{ showPottfolio.name }}
+          </div>
+          <div class="hover-container-wrapper-desc">
+            {{ showPottfolio.description }}
+          </div>
+          <div class="hover-container-wrapper-date">
+            {{ showPottfolio.tags[0] }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -229,7 +331,9 @@ export default {
         { id: 'packing', name: "Packing" },
         { id: 'advertisement', name: "Advertisement" }
       ],
-      tableData: []
+      tableData: [],
+      showHover: false,
+      showPottfolio: {}
     }
   },
   computed: {
@@ -268,7 +372,6 @@ export default {
       slidesPerView: 5,
       spaceBetween: 20
     });
-
   },
   methods: {
     filterPortfolio (item) {
@@ -281,6 +384,16 @@ export default {
         this.tableData = res.data;
       })
     },
+    show (item) {
+      this.showPottfolio = item;
+      this.showHover = true;
+      this.$nextTick(() => {
+        new Swiper('.swiper-container-hover');
+      })
+    },
+    closeHover () {
+      this.showHover = false;
+    }
   }
 }
 </script>
